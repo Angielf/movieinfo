7fe2f52493c00810d53468c2b2427b446cd2b0aa
Angielf <4748510+Angielf@users.noreply.github.com>
2020-01-04 23:34:27 +0300

    movie detail, style

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import date
 
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField("Category", max_length=150)
@@ -70,6 +72,12 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
+
+    def get_review(self):
+        return self.reviews_set.filter(parent__isnull=True)
 
     class Meta:
         verbose_name = "Movie"
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
+from .forms import ReviewForm
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Movies List"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = "movies/movie_list.html"
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())  # redirect to the same page
